17d2a6ab9a7a5862196f6558169114db16fae674
okodev <oko.webdev@gmail.com>
2020-05-15 23:20:08 +0300

    clamp posts + ajax posts

--- js/main.js
+++ js/main.js
@@ -2,42 +2,51 @@
 
 // let module = document.getElementById("clamp");
 // $clamp(module, {clamp: 5});
+document.addEventListener('DOMContentLoaded', () => {
 
+    const clamp = () => {
+        const modules = document.querySelectorAll('.clamp');
+        modules.forEach(module => $clamp(module, {clamp: 3}))
+    };
 
-let modules = document.querySelectorAll('.clamp');
-modules.forEach(module => $clamp(module, {clamp: 3}));
+    clamp();
 
-
-$('.load-more').on('click', function () {
-    const btn = $(this);
-    const loader = btn.find('span');
-    $.ajax({
-        url: '/data.html',
-        type: 'GET',
-        beforeSend: function () {
-            btn.attr('disabled', true);
-            loader.addClass('d-inline-block');
-        },
-        success: function (response) {
-            setTimeout(function () {
+    $('.load-more').on('click', function () {
+        const btn = $(this);
+        const loader = btn.find('span');
+        $.ajax({
+            url: '/data.html',
+            type: 'GET',
+            beforeSend: function () {
+                btn.attr('disabled', true);
+                loader.addClass('d-inline-block');
+            },
+            success: function (response) {
+                setTimeout(function () {
+                    loader.removeClass('d-inline-block');
+                    btn.attr('disabled', false);
+                    $('.after-posts').before(response);
+                    clamp();
+                }, 1000);
+            },
+            error: function () {
+                alert('Error!');
                 loader.removeClass('d-inline-block');
                 btn.attr('disabled', false);
-                $('.after-posts').before(response);
-            }, 1000);
-        },
-        error: function () {
-            alert('Error!');
-            loader.removeClass('d-inline-block');
-            btn.attr('disabled', false);
-        }
+            }
+        });
+
     });
+
+
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(response => response.json())
+//     .then(json => console.log(json));
+
+
+
 });
-
-
-
-
-
-
 
 
 
